c20ee3025533daad56b547019487f90716a68c5e
teresapcoutinho <teresa.jesus.coutinho@gmail.com>
2020-11-17 17:00:44 +0000

    Libraries and connection

--- aujezsky.R
+++ aujezsky.R
@@ -2,15 +2,44 @@
 library(DBI)
 library(RMySQL)
 library(dplyr)
+library(data.table)
+library(ggplot2)
+library(ggthemes)
+library(grid)
+library(tibble)
+library(reshape2)
+library(ggpubr)
+library(maps)
+library(ggiraph)
+library(leaflet)
+library(geojsonio)
+library(gganimate)
+library(plotly)
+library(RColorBrewer)
+library(sf)
+library(grid)
+library(gridExtra)
+library(htmltools)
+library(zoo)
+library(testthat)
+library(rlang)
+library(devtools)
+library(tidyr)
+library(lubridate)
+library(scales)
+library(highcharter)
+library(here)
+library(purrr)
+library(magrittr)
 
 # Connection with MySQL database
 drv <- dbDriver("MySQL")
 con <- dbConnect(drv, username = 'root', 
-                 password = 'Pinoquio1997', 
-                 dbname = 'siss', 
+                 password = 'projetoporcos', 
+                 dbname = 'Swine', 
                  host = 'localhost')
 
-## Export tables
+## Read tables (PARA QUANDO TIVERMOS AS TABELAS)
 exploracoes <- dbReadTable(con, "st_tabela_exploracoes")
 contagens <- dbReadTable(con, "st_tabela_contagens")
 vacinacoes <- dbReadTable(con, "st_tabela_vacinacoes")
