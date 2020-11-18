0a37554d4e44e2c0ab7fd7b525f1af2bc6980718
Sara Sequeira <sarasequeira5@hotmail.com>
2020-11-18 14:30:07 +0000

    organização + packages necessários

--- aujezsky.R
+++ aujezsky.R
@@ -4,34 +4,17 @@ library(RMySQL)
 
 library(dplyr)
 library(data.table)
+library(tidyr)
+library(devtools)
+library(lubridate)
 library(ggplot2)
 library(ggthemes)
-library(grid)
-library(tibble)
-library(reshape2)
-library(ggpubr)
-library(maps)
-library(ggiraph)
-library(leaflet)
-library(geojsonio)
-library(gganimate)
 library(plotly)
 library(RColorBrewer)
-library(sf)
-library(grid)
-library(gridExtra)
-library(htmltools)
-library(zoo)
-library(testthat)
-library(rlang)
-library(devtools)
-library(tidyr)
-library(lubridate)
-library(scales)
-library(highcharter)
-library(here)
-library(purrr)
-library(magrittr)
+library(gganimate)
+library(ggpubr)
+library(tibble)
+library(reshape2)
 
 # Connection with MySQL database
 connection <- dbConnect(RMariaDB :: MariaDB(),
@@ -40,9 +23,7 @@ connection <- dbConnect(RMariaDB :: MariaDB(),
                         user = "root",
                         password = "projetoporcos")
 
-## Read tables
-exploracoes <- dbReadTable(connection, "st_tabela_exploracoes")
-contagens <- dbReadTable(connection, "st_tabela_contagens")
+#Tables (a aplicar + à frente em cada tópico)
 vacinacoes <- dbReadTable(connection, "st_tabela_vacinacoes")
 classificacoes <- dbReadTable(connection, "st_tabela_classificacoes")
 controlos <- dbReadTable(connection, "st_tabela_controlos")
@@ -50,3 +31,10 @@ localizacoes <- dbReadTable(connection, "st_tabela_localizacoes")
 abates <- dbReadTable(connection, "st_tabela_abates")
 animais_abatidos <- dbReadTable(connection, "st_tabela_animais_abatidos")
 matadouros <- dbReadTable(connection, "st_tabela_matadouros")
+
+# Pig farm's distribution by LVS (local veterinary service)
+## Read tables from MySQLWorkbench
+exploracoes <- dbReadTable(connection, "st_tabela_exploracoes")
+contagens <- dbReadTable(connection, "st_tabela_contagens")
+
+
