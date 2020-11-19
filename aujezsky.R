1b0d0d29297bae239793a33933aca6e873a4f53e
Sara Sequeira <sarasequeira5@hotmail.com>
2020-11-19 14:22:18 +0000

    Organized questions

--- aujezsky.R
+++ aujezsky.R
@@ -17,7 +17,6 @@ library(tibble)
 library(reshape2)
 library(mapdeck)
 library(colourvalues)
-library(RColorBrewer)
 
 # Connection with MySQL database
 connection <- dbConnect(RMariaDB :: MariaDB(),
@@ -28,7 +27,9 @@ connection <- dbConnect(RMariaDB :: MariaDB(),
 #Mapdeck token
 token <- "pk.eyJ1IjoidGVyZXNhcGNvdXRpbmhvIiwiYSI6ImNraG9tbGRvZTBiNW8yc3A1cHgwMTM3engifQ.IZkYiF2VaRnuW9lm6h3SgQ"
 
-#Tables (a aplicar + à frente em cada tópico)
+# Read tables from MySQLWorkbench
+exploracoes <- dbReadTable(connection, "st_tabela_exploracoes")
+contagens <- dbReadTable(connection, "st_tabela_contagens")
 vacinacoes <- dbReadTable(connection, "st_tabela_vacinacoes")
 classificacoes <- dbReadTable(connection, "st_tabela_classificacoes")
 controlos <- dbReadTable(connection, "st_tabela_controlos")
@@ -38,40 +39,47 @@ animais_abatidos <- dbReadTable(connection, "st_tabela_animais_abatidos")
 matadouros <- dbReadTable(connection, "st_tabela_matadouros")
 contingencias <- dbReadTable(connection, "st_tabela_contingencias")
 
-# Pig farm's distribution by LVS (local veterinary service)
-## Read tables from MySQLWorkbench
-exploracoes <- dbReadTable(connection, "st_tabela_exploracoes")
-contagens <- dbReadTable(connection, "st_tabela_contagens")
 
-# Number of animals by farm 
+
+
+# 1 - Pig farm's distribution by LVS (local veterinary service) - HOW TO HAVE A LAYER WITH THAT?
+
+
+
+
+
+
+
+# 2 - Number of animals by farm 
 ## Table with total animals
 count <- as.data.frame(aggregate(contagens$contagem, by = list(contagens$declaracao_existencias), FUN = sum))
 count <- as.data.frame(merge(contagens, count, by.x = "declaracao_existencias", by.y = "Group.1"))
 names(count)[11] <- "total"
 count$total <- as.numeric(count$total)
 
-## Remove duplicated rows based on declaracao_existencias
+### Remove duplicated rows based on declaracao_existencias
 count <- count %>% 
   distinct(declaracao_existencias, .keep_all = TRUE)
 
-## Add column with label 
+
+## Map
+### Add column with label 
 count$info <- paste0("<br>", count$exploracao, "<br>", count$svl, " - ", count$dsavr, "<br>", count$contagem, " ", "animais", "<br>")
 
-## Select only declaracao_existencias from 2020
+### Select only declaracao_existencias from 2020
 count <- count %>%
   filter(count$data > as.Date("2020-01-01"))
 
-## Remove farms missing any information
+### Remove farms missing any information
 count <- na.omit(count)
 
-## Define categories based on total animals
+### Define categories based on total animals
 count$categoria <- cut(count$total, c(0,50,100,250,500,750,1000,2500,5000,10000,25000,50000))
 levels(count$categoria) <- c("0-50", "50-100", "100-250", "250-500", "500-750", "750-1000", "1000-2500", "2500-5000", "5000-10000", "10000-25000", "25000-50000")
 count$categoria <- as.character(count$categoria)
 
 
-
-## Map
+### Map
 mapdeck(token = token, style = mapdeck_style("light")) %>%
   add_scatterplot(data = count, 
                   lat = "latitude", 
@@ -83,3 +91,42 @@ mapdeck(token = token, style = mapdeck_style("light")) %>%
                   layer_id = "scatter_layer",
                   legend_options = list(fill_colour = list(title = "Number of animals by farm")),
                   palette = "inferno")
+
+
+
+# Percentage of pig farms currently classified (in general and farm specified)
+
+
+
+
+
+
+
+
+
+# → Tabela da prevalência, usar de alguma forma!!
+
+# Estado geral das classificações sanitárias;
+##Gráfico de barras (grouped barplot) por anos e classificações por barra (A1, A2, A3, A4, A5), divididas por tipo de exploração (stacked);
+##Mapa com várias explorações e respetiva informação (PLOTLY): nº animais, tipo exploração, classificação ...;
+##Pie chart ou semelhante com % explorações por classificação sanitária;
+
+
+#Nº de abates
+##Mapa com nº abates de 2019 por exploração e/ou por SVL;
+##Geom_line para avaliar o nº abates ao longo do tempo (mensal) // DSAVR ou SVL
+
+
+#Ensaios Laboratoriais
+##Avaliar nº positivos a DA / total de animais amostrados por SVL ou por laboratório;
+##Evolução do nº de resultados positivos / nº animais amostrados ao longo do tempo (geom_line);
+
+
+#Vacinações
+##Avaliar nº vacinados nos diferentes anos por classe (small multiple geom bar, 1 para cada ano, cada barra para a classe)
+##Nº vacinados por SVL (mapa)
+##Nº e Percentagem de animais vacinados por classificação sanitária (lolipop chart com 2 eixos, 1 para nº e 1 para %)
+##Verificação de cumprimento de prazos, intervalo entre vacinações. Se está de acordo com o estipulado para cada classificação sanitária e/ou classe (pie chart)
+
+
+
