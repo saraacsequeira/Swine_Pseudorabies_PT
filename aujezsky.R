b818c23bc2658022f6f4fa3003e07f8465e2dc31
Sara Sequeira <sarasequeira5@hotmail.com>
2020-11-19 14:27:16 +0000

    Organized

--- aujezsky.R
+++ aujezsky.R
@@ -41,16 +41,17 @@ contingencias <- dbReadTable(connection, "st_tabela_contingencias")
 
 
 
+# 1 - PIG FARM'S DISTRIBUTION
 
-# 1 - Pig farm's distribution by LVS (local veterinary service) - HOW TO HAVE A LAYER WITH THAT?
-
-
-
+## 1.1 - Pig farm's distribution by LVS (local veterinary service) - HOW TO HAVE A LAYER WITH THAT?
 
 
 
 
-# 2 - Number of animals by farm 
+
+
+
+# 1.2 - Number of animals by farm 
 ## Table with total animals
 count <- as.data.frame(aggregate(contagens$contagem, by = list(contagens$declaracao_existencias), FUN = sum))
 count <- as.data.frame(merge(contagens, count, by.x = "declaracao_existencias", by.y = "Group.1"))
@@ -94,7 +95,7 @@ mapdeck(token = token, style = mapdeck_style("light")) %>%
 
 
 
-# Percentage of pig farms currently classified (in general and farm specified)
+# 1.3 Percentage of pig farms currently classified (in general and farm specified)
 
 
 
@@ -106,23 +107,23 @@ mapdeck(token = token, style = mapdeck_style("light")) %>%
 
 # → Tabela da prevalência, usar de alguma forma!!
 
-# Estado geral das classificações sanitárias;
+# 2. Estado geral das classificações sanitárias;
 ##Gráfico de barras (grouped barplot) por anos e classificações por barra (A1, A2, A3, A4, A5), divididas por tipo de exploração (stacked);
 ##Mapa com várias explorações e respetiva informação (PLOTLY): nº animais, tipo exploração, classificação ...;
 ##Pie chart ou semelhante com % explorações por classificação sanitária;
 
 
-#Nº de abates
+# 3. Abates
 ##Mapa com nº abates de 2019 por exploração e/ou por SVL;
 ##Geom_line para avaliar o nº abates ao longo do tempo (mensal) // DSAVR ou SVL
 
 
-#Ensaios Laboratoriais
+# 4. Ensaios Laboratoriais
 ##Avaliar nº positivos a DA / total de animais amostrados por SVL ou por laboratório;
 ##Evolução do nº de resultados positivos / nº animais amostrados ao longo do tempo (geom_line);
 
 
-#Vacinações
+# 5. Vacinações
 ##Avaliar nº vacinados nos diferentes anos por classe (small multiple geom bar, 1 para cada ano, cada barra para a classe)
 ##Nº vacinados por SVL (mapa)
 ##Nº e Percentagem de animais vacinados por classificação sanitária (lolipop chart com 2 eixos, 1 para nº e 1 para %)
